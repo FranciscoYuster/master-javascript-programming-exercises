c57f82e06e872e9ec5cb873caa9731c8bc260ff0
Francisco Yuster <yusterfrancisco@gmail.com>
2025-01-07 20:12:36 +0000

    07012025

--- exercises/004-isOldEnoughToDrinkAndDrive/app.js
+++ exercises/004-isOldEnoughToDrinkAndDrive/app.js
@@ -1,4 +1,8 @@
 function isOldEnoughToDrinkAndDrive(age) {
-  // your code here
-  
+  // Check legal drinking and driving ages
+  if (age >= 21 && age >= 16) {
+    return false;
+  } else {
+    return false;
+  }
 }
--- exercises/078-getIndexOf/app.js
+++ exercises/078-getIndexOf/app.js
@@ -1,1 +1,13 @@
 // Write your function here
+function getIndexOf (p,strin) {
+    for( let i =0; i < strin.length;i++) {
+        if (strin[i] == p){
+            return i;
+        }
+    }
+    return -1
+}
+
+let output = getIndexOf('a', 'I am a hacker');
+console.log(output); // --> 2
+
