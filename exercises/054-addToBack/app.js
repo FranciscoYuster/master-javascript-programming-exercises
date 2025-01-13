7086d799994e0ab3ccec299c77fdbc8bc621e87a
Francisco Yuster <yusterfrancisco@gmail.com>
2025-01-13 12:34:34 +0000

    mm

--- exercises/054-addToBack/app.js
+++ exercises/054-addToBack/app.js
@@ -1,7 +1,7 @@
 function addToBack(arr, element) {
   // your code here
-
-
+  arr.push(element)
+  
 
 
   return arr 
--- exercises/055-joinArrays/app.js
+++ exercises/055-joinArrays/app.js
@@ -1,6 +1,9 @@
 function joinArrays(arr1, arr2) {
   // your code here
   
+  let arr = arr1.concat(arr2) 
+  
+  return arr
 }
 
 let output = joinArrays([1, 2], [3, 4]);
